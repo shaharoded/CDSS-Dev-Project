714e1ad893a608fdf5dbdc7edf1754c921602a5f
Yonatan Sinai <shahar6771@gmail.com>
2025-05-11 20:03:12 +0300

    Remove 'tests/' directory from version control

--- tests/check_patients.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('../data/cdss.db')
-cur = conn.cursor()
-cur.execute("SELECT * FROM Patients;")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-conn.close()
